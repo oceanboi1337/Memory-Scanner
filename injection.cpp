639422ac7596ef2f2a5742ddc364e215b0ddfd0a
oceanboi <oceanboi@laptop>
2021-08-08 15:32:13 +0200

    More cope

--- injection.cpp
+++ injection.cpp
@@ -2,11 +2,30 @@
 
 bool memory::process::crt_inject(const std::wstring& dll)
 {
-	void* alloc = VirtualAllocEx(this->handle, NULL, dll.size(), MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+	void* load_library = (void*)GetProcAddress(GetModuleHandle(L"kernel32.dll"), "LoadLibraryA");
+	if (load_library == NULL) {
+		perror("[ERROR]: Failed to find LoadLibraryA address.");
+		return false;
+	}
+
+	void* alloc = VirtualAllocEx(this->handle, NULL, dll.size(), MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
 	if (alloc == NULL) {
 		perror("[ERROR]: Failed to allocate memory.");
 		return false;
 	}
+
+	int bytes_written = WriteProcessMemory(this->handle, alloc, dll.data(), dll.size(), NULL);
+	if (bytes_written == 0) {
+		perror("[ERROR]: Failed to write dll path.");
+	}
+
+	void* thread = CreateRemoteThread(this->handle, NULL, 0, (LPTHREAD_START_ROUTINE)load_library, alloc, NULL, NULL);
+	if (thread == NULL) {
+		perror("[ERROR]: CreateRemoteThread failed.");
+		return false;
+	}
+
+	return true;
 }
 
 bool memory::process::inject(const std::wstring& dll, memory::injection_method method)
--- main.cpp
+++ main.cpp
@@ -4,13 +4,12 @@
 
 int main()
 {
-	memory::process proc(L"LeagueClientUx.exe");
+	memory::process proc(L"notepad.exe");
 	if (proc.open()) {
 		wprintf(L"Succesfully opened %s (%d)\n", proc.name.c_str(), proc.id);
 
-		for (const auto& address : proc.scan<int>(7093)) {
-			proc.write<int>(address, 10000);
-			std::wcout << "0x" << std::hex << address << std::endl;
+		for (const auto& address : proc.scan<int>(41)) {
+
 		}
 	}
 
